e01ed4f84658418f13771e8beb88c6c798812869
sorrynthx <sorrynthx@gmail.com>
2022-04-13 21:12:54 +0900

    2022-04-13 style-components 적용해보기

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,8 +1,33 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Father>
+      <BoxOne>
+        <Text>jh2</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
