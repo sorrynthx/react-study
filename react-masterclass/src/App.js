ace9fe75496d5b4ba25118bfc3987ccd2d7f45f8
sorrynthx <sorrynthx@gmail.com>
2022-04-13 22:26:25 +0900

    2022-04-13 마스터클래스 styled-components 애니메이션 + hover 추가(타겟을 자식 태그로 설정)

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,25 +1,49 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: tomato;
+const rotaionAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input2 = styled.input`
-
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotaionAnimation} 1s linear infinite;
+  &:hover {
+    background-color: yellow;
+  }
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 90px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input2 />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🥝</span>
+      </Box>
+    </Wrapper>
   );
 }
 
