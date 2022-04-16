116eb0f6d18818ec66add3d418fc3be8312bb98c
sorrynthx <sorrynthx@gmail.com>
2022-04-16 18:26:12 +0900

    2022-04-16 마스터클래스 타입스크립트 Form, event 타입설정

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,10 +1,34 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+
 
 function App() {
+
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+
+    console.log(value); // === event.currentTarget.value
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello ", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="hi" />
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
