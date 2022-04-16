b3b2fcb27f09a8915198e1103fe2cf8d80e3fb79
sorrynthx <sorrynthx@gmail.com>
2022-04-16 18:42:28 +0900

    2022-04-16 마스터클래스 타입스크립트를 활용한 theme 적용해보기(라이트, 다크모드)

--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
