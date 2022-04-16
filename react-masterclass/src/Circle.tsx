44c24d40f7b2364aa36ce9ab550135ae32f0c006
sorrynthx <sorrynthx@gmail.com>
2022-04-16 18:08:05 +0900

    2022-04-16 마스터클래스 타입스크립트 state 두개 설정<string | number>

--- react-masterclass/src/Circle.tsx
+++ react-masterclass/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 // Container의 props의 타입을 설명, 어떻게 보일지 정의
@@ -23,12 +24,14 @@ interface CircleProps {
 }
 
 function Circle({ bgColor, borderColor, text = "plz input text" }: CircleProps) {
-    return (
+    
+    // state 초기값이 number일수도 있고 string일수도 있음
+    const [value, setValue] = useState<number|string>(0);
+    
+    return (    
         // ?? borderColor undefined이면 bgColor 대체  
-        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} >  
-            {text}
-        </Container>
-    );
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} >{text}</Container>
+    )
 }
 
 export default Circle;
