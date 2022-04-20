93b1701c81cb6d00a2d0f5170b601ef7f214ffcb
sorrynthx <sorrynthx@gmail.com>
2022-04-20 22:00:54 +0900

    2022-04-20 코인 트래커 - 타입스크립트 - API 추가 + 로딩 추가

--- coin-tracker/src/routes/Coins.tsx
+++ coin-tracker/src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -19,61 +22,70 @@ const CoinsList = styled.ul`
 const Coin = styled.li`
     background-color: white;
     color: ${(props) => props.theme.bgColor};
-    padding: 20px;
     margin-bottom: 10px;
     border-radius: 15px;
+    a {
+        padding: 20px;
+        transition: color 0.2s ease-in;
+        display: block;
+    }
+    &:hover {
+        a {
+            color: ${(props) => props.theme.accentColor}
+        }
+    }
 `;
-
 
 const Title = styled.h1`
     font-size: 48px;
     color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [  
-    {
-        "id": "btc-bitcoin",
-        "name": "Bitcoin",
-        "symbol": "BTC",
-        "rank": 1,
-        "is_new": false,
-        "is_active": true,
-        "type": "coin"
-    },
-    {
-        "id": "eth-ethereum",
-        "name": "Ethereum",
-        "symbol": "ETH",
-        "rank": 2,
-        "is_new": false,
-        "is_active": true,
-        "type": "coin"
-    },
-    {
-        "id": "usdt-tether",
-        "name": "Tether",
-        "symbol": "USDT",
-        "rank": 3,
-        "is_new": false,
-        "is_active": true,
-        "type": "token"
-    }
-]
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 function Coins() {
+    
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async() => {
+          const response = await fetch("https://api.coinpaprika.com/v1/coins");
+          const json = await response.json();
+          setCoins(json.slice(0, 100));
+          setLoading(false);
+        })();
+    }, [])
     
     return (        
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            <CoinsList>
-                {coins.map((coin) => (
-                <Coin key={coin.id}> 
-                    <Link to={`/${coin.id}`}> {coin.name} &rarr; </Link>
-                </Coin>
-                ))}
-            </CoinsList>
+            { loading ? (<Loader>Loading...</Loader>)
+                : (
+                    <CoinsList>
+                        {coins.map((coin) => (
+                        <Coin key={coin.id}> 
+                            <Link to={`/${coin.id}`}> {coin.name} &rarr; </Link>
+                        </Coin>
+                        ))}
+                    </CoinsList>
+                  )
+            }
         </Container>
         
     );
