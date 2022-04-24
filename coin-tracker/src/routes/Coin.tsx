724a5a0861b3cd878b5385ddc148fccd676797b3
sorrynthx <sorrynthx@gmail.com>
2022-04-24 10:55:42 +0900

    2022-04-24 코인 트래커 - 타입스크립트 - useEffect - API 호출 + await fetch + DataType 정의

--- coin-tracker/src/routes/Coin.tsx
+++ coin-tracker/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -25,8 +25,8 @@ const Loader = styled.span`
     display: block;
 `;
 
-interface RouteParams {
-    coinId: string,
+interface RouteParam {
+    coinId: string;
 }
 
 interface RouteState {
@@ -35,13 +35,94 @@ interface RouteState {
     }
 }
 
+// 태그는 object로 나오나 실제로 Array이기 때문에 직접 interface 추가
+interface ITag {
+    coin_counter: number,
+    ico_counter: number,
+    id: string,
+    name: string,
+}
+
+interface InfoData {
+    id:string;
+    name:string;
+    symbol:string;
+    rank:number;
+    is_new:boolean;
+    is_active:boolean;
+    type:string;
+    tags:ITag[];
+    team:object;
+    description:string;
+    message:string;
+    open_source:boolean;
+    started_at:string;
+    development_status:string;
+    hardware_wallet:boolean;
+    proof_type:string;
+    org_structure:string;
+    hash_algorithm:string;
+    links:object;
+    links_extended:object;
+    hitepaper:object;
+    first_data_at:string;
+    ast_data_at:string;
+}
+
+interface PriceData {
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
 function Coin() {
     
     const [loading, setLoading] = useState(true);
     // Coin.tsx에서 param 받기
-    const {coinId} = useParams();
+    const {coinId} = useParams() as unknown as RouteParam;
     // Coin.tsx에서 state 받기
     const {state} = useLocation() as RouteState;
+
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+    useEffect(() => {
+       return () => {
+           (async () => {
+              const infoData = await (
+                                       await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+                                   ).json();
+               console.log(infoData);     
+   
+               const priceData = await (
+                                       await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+                                   ).json();
+               console.log(priceData);
+   
+               setInfo(infoData);
+               setPriceInfo(priceData);
+               setLoading(false);
+          })();
+       }
+    }, []);
 
     return (
         <Container>
--- coin-tracker/src/routes/Coins.tsx
+++ coin-tracker/src/routes/Coins.tsx
@@ -69,12 +69,14 @@ function Coins() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        (async() => {
-          const response = await fetch("https://api.coinpaprika.com/v1/coins");
-          const json = await response.json();
-          setCoins(json.slice(0, 100));
-          setLoading(false);
-        })();
+        return () => {
+            (async() => {
+              const response = await fetch("https://api.coinpaprika.com/v1/coins");
+              const json = await response.json();
+              setCoins(json.slice(0, 100));
+              setLoading(false);
+            })();
+        }
     }, [])
     
     return (        
