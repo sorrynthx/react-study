6a3880c5b84682d9da8230b482488ad1ad18710d
sorrynthx <sorrynthx@gmail.com>
2022-05-10 21:23:57 +0900

    2022-05-10 To-Do-App - 타입스크립트 - react-hook-form useForm 데이터 타입 설정 + formState:{errors} 에러 메세지 표현

--- to-do-app/src/ToDoList.tsx
+++ to-do-app/src/ToDoList.tsx
@@ -30,13 +30,30 @@ import {useForm} from "react-hook-form";
 //     );
 // }
 
+interface IForm {
+    name?: string,  // 필수가 아닐때, ? 물음표 추가
+    email: string,
+    password: string,
+    password_confirm: string
+}
+
 function ToDoList() {
-    const {register, watch, handleSubmit, formState} = useForm();
+    const {
+        register, 
+        watch, 
+        handleSubmit, 
+        formState:{errors}
+    } = useForm<IForm>({
+        defaultValues: {
+            email: "@naver.com",
+            name: "홍길동",
+        },
+    });
     const onValid = (data:any) => {
         console.log(data);
     }
     //console.log(watch());
-    console.log(formState.errors);
+    console.log(errors);
 
     return (
         <div>
@@ -48,16 +65,35 @@ function ToDoList() {
                     {...register("email", 
                                     {
                                         required: "Eamil 필수", 
+                                        pattern: {
+                                            value: /^[a-zA-Z0-9._%+-]+@naver.com$/,
+                                            message: "이메일 형식!"
+                                        },
                                         minLength: {
-                                        value: 5, 
-                                        message: "최소 5자 이상"}
+                                            value: 5, 
+                                            message: "최소 5자 이상"
+                                        }
                                     }
                                 )
-                    } placeholder="write a Email" /><br/>
+                    } placeholder="write a Email" 
+                />
+                <span>
+                    {errors?.email?.message}
+                </span>
+                <br/>
                 
-                <input {...register("name", {required: true})} placeholder="write a Name" /><br/>
-                <input {...register("password", {required: true})} placeholder="write a Password" /><br/>
-                <input {...register("password_confirm", {required: true})} placeholder="write a Password Confirm" /><br/>
+                <input {...register("name", {required: "이름 필수!"})} placeholder="write a Name" /><br/>
+                <span>
+                    {errors?.name?.message}
+                </span>
+                <input {...register("password", {required: "비밀번호 필수!"})} placeholder="write a Password" /><br/>
+                <span>
+                    {errors?.password?.message}
+                </span>
+                <input {...register("password_confirm", {required: "비밀번호 컨펌 필수!"})} placeholder="write a Password Confirm" /><br/>
+                <span>
+                    {errors?.password_confirm?.message}
+                </span>
                 <button>Add</button>
             </form>
         </div>
