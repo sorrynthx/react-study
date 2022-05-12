2313167fef3b3d57224945f717c88df93ec8fc8d
sorrynthx <sorrynthx@gmail.com>
2022-05-12 23:22:25 +0900

    2022-05-12 To-Do-App - 타입스크립트 - react-hook-form 리액트 훅 폼 (*오류로 인해 재업로드)

---
+++ react-hook-form-ex/src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#130f40",
+    textColor: "white",
+    accentColor: "#9c88ff",
+    cardBgColor: "transparent",
+};
