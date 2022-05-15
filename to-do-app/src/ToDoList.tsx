3f2ce73f56d76f8724b65188c403b8a60bacaea3
sorrynthx <sorrynthx@gmail.com>
2022-05-15 22:59:26 +0900

    2022-05-15 error issue -> 집 PC node_module 재설치

--- to-do-app/src/ToDoList.tsx
+++ to-do-app/src/ToDoList.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
@@ -19,17 +18,19 @@ function ToDoList() {
     console.log(watch(), 'watching...' );
     
     return (
+        
         <div>
             <form>
-            <input {...register("email")} placeholder="Email" />
-            <input {...register("firstName")} placeholder="First Name" />
-            <input {...register("lastName")} placeholder="Last Name" />
-            <input {...register("username")} placeholder="Username" />
-            <input {...register("password")} placeholder="Password" />
-            <input {...register("password1")} placeholder="Password1" />
-            <button>Add</button>
+                <input {...register("email")} placeholder="Email" />
+                <input {...register("firstName")} placeholder="First Name" />
+                <input {...register("lastName")} placeholder="Last Name" />
+                <input {...register("username")} placeholder="Username" />
+                <input {...register("password")} placeholder="Password" />
+                <input {...register("password1")} placeholder="Password1" />
+                <button>Add</button>
             </form>
         </div>
+        
     );
 }
 
