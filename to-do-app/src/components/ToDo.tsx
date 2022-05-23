eecbb002092b67258b88c0847521d98536017e69
sorrynthx <sorrynthx@gmail.com>
2022-05-23 21:08:39 +0900

    2022-05-23 To-Do -> 상태변경(Doing, Done) + 선택자 인덱스 찾기 + 새로운 배열로 반환

--- to-do-app/src/components/ToDo.tsx
+++ to-do-app/src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDo } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo(data:IToDo) {
+function ToDo({text, category, id}:IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        // 변경할 카테고리
+        const {
+            currentTarget: {name},
+        } = event;
+
+        setToDos((oldToDos) => {
+            // 인덱스 find
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            // 새로운 toDo + 변경할 category
+            const newToDo = {text, id, category: name as any};
+            
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                newToDo, 
+                ...oldToDos.slice(targetIndex+1)
+            ];
+        })
+    };
     return (
         <li> 
-            <span>{data.text}</span>
-            <button>TO_DO</button>
-            <button>DOING</button>
-            <button>DONE</button>
+            <span>{text}</span>
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>DOING</button>
+            )}
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>TO_DO</button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>DONE</button>
+            )}
         </li>
     );
 }
--- to-do-app/src/components/ToDoList.tsx
+++ to-do-app/src/components/ToDoList.tsx
@@ -6,7 +6,7 @@ import ToDo from "./ToDo";
 
 function ToDoList() {
     const toDos = useRecoilValue(toDoState);
-    
+    //console.log(toDos);
     return (
         <div>
             <h1> To Dos</h1> 
