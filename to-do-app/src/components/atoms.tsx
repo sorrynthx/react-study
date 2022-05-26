164b2e3044e2f7f1f38ca803478e7fdb69604910
sorrynthx <sorrynthx@gmail.com>
2022-05-26 21:13:48 +0900

    2022-05-26 To-Do -> 상태 state 변경없이 selector 이용해서 분류 후 값 리턴 1

--- to-do-app/src/components/atoms.tsx
+++ to-do-app/src/components/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
     text: string;
@@ -9,6 +9,18 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>(
     {
         key: "toDo", 
-        default: []
+        default: [],
     }
 );
+
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({get}) => {
+        const toDos = get(toDoState)
+        return [
+            toDos.filter((toDo) => toDo.category === "TO_DO"),
+            toDos.filter((toDo) => toDo.category === "DOING"),
+            toDos.filter((toDo) => toDo.category === "DONE"),
+        ];
+    },
+});
