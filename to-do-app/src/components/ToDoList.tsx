b9d2fe213ace6282aa7d836309c78f46b7d3e648
sorrynthx <sorrynthx@gmail.com>
2022-05-26 21:37:03 +0900

    2022-05-26 To-Do -> 상태 state 변경없이 selector 이용해서 분류 후 값 리턴 2

--- to-do-app/src/components/ToDoList.tsx
+++ to-do-app/src/components/ToDoList.tsx
@@ -1,38 +1,29 @@
 
-import { useRecoilValue} from "recoil";
-import { toDoSelector, toDoState } from "./atoms";
+import React from "react";
+import { useRecoilState, useRecoilValue} from "recoil";
+import { categoryState, toDoSelector, toDoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
     
-    const [toDo, doing, done] = useRecoilValue(toDoSelector);
-    
+    const toDos = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState);
+    const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+        setCategory(event.currentTarget.value);
+    };
     return (
         <div>
             <h1> To Dos</h1> 
             < hr /> 
             <CreateToDo /> 
-            <h2>To Do</h2>
-            <ul> 
-                {toDo.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
-            <hr/>
-            <h2>Doing</h2>
-            <ul> 
-                {doing.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
-            <hr/>
-            <h2>Done</h2>
-            <ul> 
-                {done.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
+            
+            <select value={category} onInput={onInput}>
+                <option value="TO_DO">TO DO</option>
+                <option value="DOING">DOING</option>
+                <option value="DONE">DONE</option>
+            </select>
+            {toDos?.map(toDo => <ToDo key={toDo.id} {...toDo} />) }
         </div>
     );
 }
