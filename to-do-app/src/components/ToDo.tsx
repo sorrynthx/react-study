fa9fe1af693625aad23a8b84147f6a1938803b55
sorrynthx <sorrynthx@gmail.com>
2022-05-30 21:18:46 +0900

    2022-05-30 To-Do -> 현재 상태 state로 todo 등록 + ENUM 사용

--- to-do-app/src/components/ToDo.tsx
+++ to-do-app/src/components/ToDo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "./atoms";
+import { Categories, IToDo, toDoState } from "./atoms";
 
 function ToDo({text, category, id}:IToDo) {
     const setToDos = useSetRecoilState(toDoState);
@@ -27,14 +27,14 @@ function ToDo({text, category, id}:IToDo) {
     return (
         <li> 
             <span>{text}</span>
-            {category !== "DOING" && (
-                <button name="DOING" onClick={onClick}>DOING</button>
+            {category !== Categories.DOING && (
+                <button name={Categories.DOING} onClick={onClick}>DOING</button>
             )}
-            {category !== "TO_DO" && (
-                <button name="TO_DO" onClick={onClick}>TO_DO</button>
+            {category !== Categories.TO_DO && (
+                <button name={Categories.TO_DO} onClick={onClick}>TO_DO</button>
             )}
-            {category !== "DONE" && (
-                <button name="DONE" onClick={onClick}>DONE</button>
+            {category !== Categories.DONE && (
+                <button name={Categories.DONE} onClick={onClick}>DONE</button>
             )}
         </li>
     );
--- to-do-app/src/components/ToDoList.tsx
+++ to-do-app/src/components/ToDoList.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { useRecoilState, useRecoilValue} from "recoil";
-import { categoryState, toDoSelector, toDoState } from "./atoms";
+import { Categories, categoryState, toDoSelector, toDoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -10,7 +10,7 @@ function ToDoList() {
     const toDos = useRecoilValue(toDoSelector);
     const [category, setCategory] = useRecoilState(categoryState);
     const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-        setCategory(event.currentTarget.value);
+        setCategory(event.currentTarget.value as any);
     };
     return (
         <div>
@@ -19,9 +19,9 @@ function ToDoList() {
             <CreateToDo /> 
             
             <select value={category} onInput={onInput}>
-                <option value="TO_DO">TO DO</option>
-                <option value="DOING">DOING</option>
-                <option value="DONE">DONE</option>
+                <option value={Categories.TO_DO}>TO DO</option>
+                <option value={Categories.DOING}>DOING</option>
+                <option value={Categories.DONE}>DONE</option>
             </select>
             {toDos?.map(toDo => <ToDo key={toDo.id} {...toDo} />) }
         </div>
