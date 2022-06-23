c6c3b89f62f4d7667cd69c37fab64b1ddf991e19
sorrynthx <sorrynthx@gmail.com>
2022-06-23 10:14:15 +0900

    2022-06-23 React Animation 설치 및 기초 사용법(Framer animation)

---
+++ animation/src/App.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import {motion} from 'framer-motion'
+
+const Wrapper = styled.div `
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVars = {
+  start: {scale: 0},
+  end: {
+        scale: 1, 
+        rotateZ: 360, 
+        transition: {type: "spring", bounce: 0.5}
+      }
+}
+
+function App() {
+    return (
+      <Wrapper>
+        <Box 
+          variants={myVars} 
+          initial="start"
+          animate="end"
+        />
+      </Wrapper>
+  );
+}
+
+export default App;
