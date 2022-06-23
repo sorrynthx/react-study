28ab0456b231d4646bc591ac8e9a147e9c64e3cf
sorrynthx <sorrynthx@gmail.com>
2022-06-23 11:16:22 +0900

    2022-06-23 React Animation 자식요소 애니메이션 컨트롤

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -2,15 +2,60 @@ import styled from "styled-components";
 import {motion} from 'framer-motion'
 
 const Wrapper = styled.div `
-  display: flex;
   height: 100vh;
   width: 100vw;
   margin: 0 auto;
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
   height: 100vh;
 `;
+
+const Box2 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const box2Vars = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+const circleVariants  = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  } 
+};
+
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -19,7 +64,6 @@ const Box = styled(motion.div)`
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
 const myVars = {
   start: {scale: 0},
   end: {
@@ -32,6 +76,14 @@ const myVars = {
 function App() {
     return (
       <Wrapper>
+        
+        <Box2 variants={box2Vars} initial="start" animate="end">
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+        </Box2>
+                
         <Box 
           variants={myVars} 
           initial="start"
@@ -42,3 +94,4 @@ function App() {
 }
 
 export default App;
+
