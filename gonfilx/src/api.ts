b47dca8d07d86110b4b8747817f9265fc3ba9127
sorrynthx <sorrynthx@gmail.com>
2022-08-03 18:49:53 +0900

    2022-08-02 React 넷플릭스 클론코딩 홈2 - React Query + interface 타입 정의 후 홈 배경 설정

--- gonfilx/src/api.ts
+++ gonfilx/src/api.ts
@@ -1,6 +1,26 @@
 const API_KEY = 'bc51ec97a9e2a30b346298351d4167e5';
 const BASE_PATH = 'https://api.themoviedb.org/3';
 
+interface IMovice {
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    overview: string;
+}
+
+export interface IGetMovieResult {
+    dates: {
+        maximum: string;
+        minimum: string;
+    };
+    page: number,
+    results: IMovice[],
+    total_pages: number,
+    total_results: number,
+
+}
+
 export function getMovies() {
     return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`)
     .then((response) => response.json());
